7fa3375569f9b0e70a7ba0c8dbb16027bcb4cb55
Bonk2022 <104210131+Bonk2022@users.noreply.github.com>
2023-05-31 13:46:29 +0200

    Update Hoofdletters

--- src/klant.inc.php
+++ src/klant.inc.php
@@ -1,7 +1,7 @@
 <?php include 'userfunction.php'?>
 <?php
             $naam = $_POST['voornaam'];
-            $naam = ucfirst($naam); // Capitalize the first letter of the input
+            
             $tussenvoegsel = $_POST['tussenvoegsel'];
             $achternaam = $_POST['achternaam'];
             $mobielnummer = $_POST['mobielnummer'];
@@ -9,6 +9,16 @@
             $email = $_POST['email'];
             $adres = $_POST['addres'];
             $postcode = $_POST['postcode'];
+
+            // Invoersels naar hoofdletters
+            $naam = ucfirst($naam); 
+            $tussenvoegsel = ucfirst($tussenvoegsel);
+            $achternaam = ucfirst($achternaam);
+            $adres = ucfirst($adres);
+      	$postcode = $_POST['postcode'];
+            $letters = substr($postcode, -2); // Krijgt de laatste 2 letters van de postcode
+            $letters = strtoupper($letters); // Maakt de letters hoofdletters
+            $postcode = substr_replace($postcode, $letters, -2); // herplaatst de letters naar Hoofdletters
 
             $sql = "INSERT INTO klant (naam, tussenvoegsel ,achternaam, mobielnummer, vastnummer , email, adres, postcode) VALUES ('$naam','$tussenvoegsel', '$achternaam', '$mobielnummer', '$vastnummer', '$email', '$adres', '$postcode')";
             if (mysqli_query($conn, $sql)) {
