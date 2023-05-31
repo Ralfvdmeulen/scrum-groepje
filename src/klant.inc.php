15cd072878b995140257a35e845f622ffd2bb0a7
Bonk2022 <104210131+Bonk2022@users.noreply.github.com>
2023-05-31 13:42:43 +0200

    hoofdletters

--- src/klant.inc.php
+++ src/klant.inc.php
@@ -1,6 +1,7 @@
 <?php include 'userfunction.php'?>
 <?php
             $naam = $_POST['voornaam'];
+            $naam = ucfirst($naam); // Capitalize the first letter of the input
             $tussenvoegsel = $_POST['tussenvoegsel'];
             $achternaam = $_POST['achternaam'];
             $mobielnummer = $_POST['mobielnummer'];
